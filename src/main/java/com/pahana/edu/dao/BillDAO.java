7558161a55a121b028001694ef40c13bea7ca2e9
Sadeesha <mrsadeesha@gmail.com>
2025-08-29 21:28:15 +0530

    Update BillDAO

--- src/main/java/com/pahana/edu/dao/BillDAO.java
+++ src/main/java/com/pahana/edu/dao/BillDAO.java
@@ -39,8 +39,7 @@ public class BillDAO {
             billStmt.setString(7, bill.getPaymentMethod());
             billStmt.setString(8, bill.getPaymentStatus());
             billStmt.setInt(9, bill.getCreatedBy());
-            billStmt.setString(10, bill.getNotes());
-
+            
             int billRows = billStmt.executeUpdate();
 
             if (billRows == 0) {
